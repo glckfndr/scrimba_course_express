f370795f0624fdef27d410a2e302d6e6ebdff7ed
glckfndr <obulan65@gmail.com>
2025-09-30 00:03:36 +0300

    feat: add handling unknown route

--- server.js
+++ server.js
@@ -11,5 +11,12 @@ Challenge:
 const app = express();
 
 app.use("/api", apiRouter);
+app.use((req, res) => {
+  res
+    .status(404)
+    .json({
+      message: "Endpoint not found. Please check the API documentation.",
+    });
+});
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
