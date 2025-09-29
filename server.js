860c330b99967300b3eb41679f4b37b25766da74
glckfndr <obulan65@gmail.com>
2025-09-29 07:08:19 +0300

    feat: add path parameters for get /api

--- server.js
+++ server.js
@@ -5,48 +5,18 @@ const PORT = 8000;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  let filteredData = startups;
-  /*
+/*
 Challenge:
-1. When a user hits the /api endpoint with query params, filter the data so
-we only serve objects that meet their requirements.
+1. Update the code so a GET request to api/metals/gold
+    logs an object {category: ‘metals’, type: ‘gold’}
 
-The user can filter by the following properties:
-  industry, country, continent, is_seeking_funding, has_mvp
-
-Test Cases
-
-/api?industry=renewable%20energy&country=germany&has_mvp=true
-  Should get the "GreenGrid Energy" object.
-
-/api?industry=renewable%20energy&country=germany&has_mvp=false
-  Should not get any object
-
-/api?continent=asia&is_seeking_funding=true&has_mvp=true
-  should get for objects with IDs 3, 22, 26, 29
+But a GET request to api/crypto/eth
+    logs an object {category: crypto-name, type: eth}
 */
-  const { industry, country, continent, is_seeking_funding, has_mvp } =
-    req.query;
 
-  res.json(
-    filteredData.filter(
-      (item) =>
-        (industry
-          ? item.industry.toLowerCase() === industry.toLowerCase()
-          : true) &&
-        (country
-          ? item.country.toLowerCase() === country.toLowerCase()
-          : true) &&
-        (continent
-          ? item.continent.toLowerCase() === continent.toLowerCase()
-          : true) &&
-        (is_seeking_funding
-          ? item.is_seeking_funding.toString() === is_seeking_funding
-          : true) &&
-        (has_mvp ? item.has_mvp.toString() === has_mvp : true)
-    )
-  );
+app.get("/api/:category/:type", (req, res) => {
+  console.log(req.params);
+  res.json();
 });
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
