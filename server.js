8bc20969c1525b100b2fcfb7f0a4481a4555e303
glckfndr <obulan65@gmail.com>
2025-09-30 00:11:21 +0300

    feat: add cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { apiRouter } from "./routes/apiRouter.js";
+import cors from "cors";
 
 const PORT = 8000;
 
@@ -9,14 +10,12 @@ Challenge:
 */
 
 const app = express();
-
+app.use(cors());
 app.use("/api", apiRouter);
 app.use((req, res) => {
-  res
-    .status(404)
-    .json({
-      message: "Endpoint not found. Please check the API documentation.",
-    });
+  res.status(404).json({
+    message: "Endpoint not found. Please check the API documentation.",
+  });
 });
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
